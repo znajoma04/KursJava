86d1e7448286f2aae2be8ca0439babf90185c82f
Sylwia Nowak <sylwia.katarzyna.nowak@gmail.com>
2022-11-26 18:19:08 +0100

    Pierwszy

---
+++ Intro/src/main/java/FirstClass.java
@@ -0,0 +1,8 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello word");
+        System.out.println("Sylwia");
+        System.out.println(3);
+        System.out.println('N');
+    }
+}
